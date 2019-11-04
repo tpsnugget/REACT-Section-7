a0f877f3ee96d575a50c66fa26df33c794ed7edf
Michael Giebner <mikegiebner@gmail.com>
2019-11-04 12:07:25 -0500

    # Repo: REACT-Section-7 # Repo Description: Colt Steele's Modern React Bootcamp, Section 7, React State Dice Exercise    - Something is wrong with my animation code, but not worth spending itme on that right now

--- dice/src/App.js
+++ dice/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <RollDice />
-      <RollDice />
     </div>
   );
 }
--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -1,17 +1,39 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Die from "./Die"
+import "./RollDice.css"
 
-class RollDice extends Component{
-   constructor(props){
+class RollDice extends Component {
+   constructor(props) {
       super(props)
       this.state = {
-         
+         die1: 1,
+         die2: 2,
+         rolling: false
       }
+      this.dieRoll = this.dieRoll.bind(this)
    }
-   
-   render(){
-      var dieRoll = Math.floor(Math.random() * 6) + 1
-      return <Die face={dieRoll} />
+   dieRoll() {
+      this.setState({
+         die1: Math.floor(Math.random() * 6) + 1,
+         die2: Math.floor(Math.random() * 6) + 1,
+         rolling: true
+      })
+      setTimeout(() => {
+         this.setState({ rolling: false })
+      }, 1000)
+   }
+
+
+   render() {
+      return (
+         <div className="RollDice">
+            <span><Die face={this.state.die1} rolling={this.state.rolling} /></span>
+            <span><Die face={this.state.die2} rolling={this.state.rolling} /></span>
+            <div><button className="RollDice-button" onClick={this.dieRoll} disabled={this.state.rolling}>
+               {this.state.rolling ? "Rolling..." : "Roll Dice"}
+            </button></div>
+         </div>
+      )
    }
 }
 
